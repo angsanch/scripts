a426ce569eac9f6bdae68006238d710f16809a07
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.edu>
2023-11-05 20:43:03 +0100

    FEAT: Dont show errors of ignored files

--- epitech_norminette.sh
+++ epitech_norminette.sh
@@ -4,6 +4,7 @@ basepath=$(dirname $(readlink -f $0))
 dir=$(pwd)
 save_dir=$basepath
 file_name="coding-style-reports.log"
+file=$save_dir/$file_name
 
 if [ ! -z $1 ]
 then
@@ -13,8 +14,11 @@ fi
 echo $dir $save_dir
 $basepath/epitech_coding-style.sh $dir $save_dir | awk 'END{printf "%s", $0}'
 echo
-echo
-cat $save_dir/$file_name | awk -v basepath=$basepath '{
+echo 
+ignored=$(git check-ignore $(cat $file | cut -d ':' -f1) | sed "s/ /\\|/")
+
+cat $file | grep -v "$ignored" |\
+awk -v basepath=$basepath '{
 	get_code = "echo " $0 " | rev | cut -d \":\" -f1 | rev"
 	get_code | getline code
 	get_name = "cat " basepath "/coding-style-errors | grep \"^" code "\" | cut -d \":\" -f2" 
@@ -22,4 +26,4 @@ cat $save_dir/$file_name | awk -v basepath=$basepath '{
 	printf "%-50s %s\n", $0, name}
 	close (get_code)
 	close (get_name)'
-rm -f $save_dir/$file_name
+rm -f $file
