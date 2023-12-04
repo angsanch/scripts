8ea4aa5d343902cb67db251c9e6c1ecba1315331
user <angel-daniel.sanchez-garcia@epitech.eu>
2023-12-04 03:45:00 +0100

    FEAT: try to use dnf or apt for csfml

--- install_csfml.sh
+++ install_csfml.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-sudo dnf install CSFML-devel
+if command -v dnf &> /dev/null; then
+	echo "Using dnf"
+	sudo dnf install CSFML-devel
+elif command -v apt &> /dev/null; then
+	echo "Using apt"
+	sudo apt install libcsfml-dev
+else
+	echo "No supported package manager found"
+fi
