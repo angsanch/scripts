b26b1e6ff12e40f58f053c395e86471e834173d3
user <angel-daniel.sanchez-garcia@epitech.eu>
2023-12-09 12:39:13 +0100

    FEAT: 42 norminette

--- rc.sh
+++ rc.sh
@@ -8,3 +8,4 @@ alias banana=$basepath/epitech_norminette.sh
 alias header=$basepath/epitech_header.sh
 alias fcreate=$basepath/epitech_file_create.sh
 alias gidentity=$basepath/git_identity.sh
+alias norminette="python3 -m norminette"
