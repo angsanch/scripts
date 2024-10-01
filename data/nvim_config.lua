6569c5dec4e22d3b057224a589be4f980bef4751
user <angel-daniel.sanchez-garcia@epitech.eu>
2024-10-01 16:54:26 +0200

    FEAT: add nvim command to set styles

--- data/nvim_config.lua
+++ data/nvim_config.lua
@@ -1,5 +1,45 @@
 require "nvchad.options"
 
+vim.api.nvim_create_user_command(
+  'Style',
+  function(opts)
+	local filename = vim.fn.expand('%:t')
+
+	-- Set options
+	vim.opt.wrap = false
+	vim.opt.autoindent = true
+	vim.opt.tabstop = 4 -- Size of tabs
+	vim.opt.shiftwidth = 4 -- Autoindent size
+
+	identity = opts.args
+	if identity == "epitech" then
+		if filename ~= "Makefile" then
+			vim.opt.expandtab = true -- Use spaces instead of tabs
+		end
+		vim.opt.softtabstop = 4 -- Group delete spaces in tabs
+	elseif identity == "42" then
+		vim.opt.expandtab = false -- Use tabs instead of spaces
+	elseif identity == "personal" then
+		vim.opt.expandtab = false -- Use tabs instead of spaces
+	else
+		error("Not found")
+	end
+
+	-- Set colorcolumn based on identity
+	if identity == "epitech" then
+		vim.opt.colorcolumn = "80"
+	elseif identity == "42" then
+		vim.opt.colorcolumn = "81"
+	else
+		vim.opt.colorcolumn = ""
+	end
+
+	vim.notify("Settings for " .. identity)
+	end,
+  { nargs = 1 }
+)
+
+
 -- Get the user's identity from git config
 local identity = vim.fn.system('git config user.identity'):gsub("\n", "")
 
@@ -9,28 +49,4 @@ if identity == "42" then
 	vim.g.mail42 = vim.fn.system('git config user.email'):gsub("\n", "")
 end
 
-local filename = vim.fn.expand('%:t')
-
--- Set options
-vim.opt.wrap = false
-vim.opt.autoindent = true
-vim.opt.tabstop = 4 -- Size of tabs
-
-if identity == "epitech" then
-    vim.opt.shiftwidth = 4 -- Autoindent size
-    if filename ~= "Makefile" then
-        vim.opt.expandtab = true -- Use spaces instead of tabs
-    end
-    vim.opt.softtabstop = 4 -- Group delete spaces in tabs
-elseif identity == "42" then
-    vim.opt.shiftwidth = 4 -- Autoindent size
-    vim.opt.expandtab = false -- Use tabs instead of spaces
-end
-
--- Set colorcolumn based on identity
-if identity == "epitech" then
-    vim.opt.colorcolumn = "80"
-elseif identity == "42" then
-    vim.opt.colorcolumn = "81"
-end
-
+pcall(vim.cmd("Style " .. identity))
