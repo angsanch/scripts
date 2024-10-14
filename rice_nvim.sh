30e21a3889151c37d2f8688fcabc6db46193e853
Angel Daniel Sanchez Garcia <adanielsanchezgarcia@gmail.com>
2024-10-14 05:14:59 +0200

    FEAT: full reset nvim when riceing

--- rice_nvim.sh
+++ rice_nvim.sh
@@ -3,6 +3,8 @@ basepath=$(dirname $(readlink -f $0))
 
 data_dir=$basepath/data
 
+rm -rf ~/.local/share/nvim ~/.cache/nvim
+
 cat $data_dir/nvim_config.lua > ~/.config/nvim/lua/options.lua
 
 git clone https://github.com/NvChad/starter ~/.config/nvim/ || echo $(cd ~/.config/nvim && git pull origin main)
